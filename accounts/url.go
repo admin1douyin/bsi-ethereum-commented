ca0aa45a0769064aba6ad8fb154176cd7e6cf7e1
nidaye0525 <nidaye0525@gmail.com>
2026-01-29 03:39:49 +0000

    docs: add Chinese comments to accounts/url.go

--- accounts/url.go
+++ accounts/url.go
@@ -35,12 +35,22 @@ import (
 //
 // As such, these URLs should not be used outside of the scope of an Ethereum
 // wallet or account.
+// URL 表示钱包或帐户的规范标识 URL。
+//
+// 它是 url.URL 的简化版本，具有重要的限制（在此视为功能）：
+// 它仅包含可值复制的组件，并且不对特殊字符进行任何 URL 编码/解码。
+//
+// 前者对于允许在不留下对原始版本的实时引用情况下复制帐户非常重要，
+// 而后者对于确保单一的规范形式而不是 RFC 3986 规范允许多种形式非常重要。
+//
+// 因此，这些 URL 不应在以太坊钱包或帐户范围之外使用。
 type URL struct {
-	Scheme string // Protocol scheme to identify a capable account backend
-	Path   string // Path for the backend to identify a unique entity
+	Scheme string // Protocol scheme to identify a capable account backend // 协议方案以标识有能力的帐户后端
+	Path   string // Path for the backend to identify a unique entity // 后端标识唯一实体的路径
 }
 
 // parseURL converts a user supplied URL into the accounts specific structure.
+// parseURL 将用户提供的 URL 转换为特定于帐户的结构。
 func parseURL(url string) (URL, error) {
 	parts := strings.Split(url, "://")
 	if len(parts) != 2 || parts[0] == "" {
@@ -53,6 +63,7 @@ func parseURL(url string) (URL, error) {
 }
 
 // String implements the stringer interface.
+// String 实现 stringer 接口。
 func (u URL) String() string {
 	if u.Scheme != "" {
 		return fmt.Sprintf("%s://%s", u.Scheme, u.Path)
@@ -61,6 +72,7 @@ func (u URL) String() string {
 }
 
 // TerminalString implements the log.TerminalStringer interface.
+// TerminalString 实现 log.TerminalStringer 接口。
 func (u URL) TerminalString() string {
 	url := u.String()
 	if len(url) > 32 {
@@ -70,11 +82,13 @@ func (u URL) TerminalString() string {
 }
 
 // MarshalJSON implements the json.Marshaller interface.
+// MarshalJSON 实现 json.Marshaller 接口。
 func (u URL) MarshalJSON() ([]byte, error) {
 	return json.Marshal(u.String())
 }
 
 // UnmarshalJSON parses url.
+// UnmarshalJSON 解析 url。
 func (u *URL) UnmarshalJSON(input []byte) error {
 	var textURL string
 	err := json.Unmarshal(input, &textURL)
@@ -95,6 +109,12 @@ func (u *URL) UnmarshalJSON(input []byte) error {
 //	-1 if x <  y
 //	 0 if x == y
 //	+1 if x >  y
+//
+// Cmp 比较 x 和 y 并返回：
+//
+//	-1 如果 x <  y
+//	 0 如果 x == y
+//	+1 如果 x >  y
 func (u URL) Cmp(url URL) int {
 	if u.Scheme == url.Scheme {
 		return strings.Compare(u.Path, url.Path)
