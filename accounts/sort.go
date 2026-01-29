0876f7a5465193f66c6f860897524bfe29e1f8ee
nidaye0525 <nidaye0525@gmail.com>
2026-01-29 09:03:00 +0000

    feat: add Chinese comments to accounts/sort.go and accounts/url.go

--- accounts/sort.go
+++ accounts/sort.go
@@ -14,20 +14,37 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// 版权所有 2018 The go-ethereum Authors
+// 此文件是 go-ethereum 库的一部分。
+//
+// go-ethereum 库是免费软件：您可以根据自由软件基金会发布的 GNU 宽通用公共许可证的条款重新分发和/或修改它，
+// 可以是许可证的第 3 版，也可以是（由您选择）任何更高版本。
+//
+// go-ethereum 库的发布是希望它能有用，但没有任何保证；甚至没有对适销性或特定用途适用性的默示保证。
+// 有关更多详细信息，请参阅 GNU 宽通用公共许可证。
+//
+// 您应该已经随 go-ethereum 库收到一份 GNU 宽通用公共许可证的副本。如果没有，请参阅 <http://www.gnu.org/licenses/>。
+
 package accounts
 
 // AccountsByURL implements sort.Interface for []Account based on the URL field.
 // AccountsByURL 基于 URL 字段为 []Account 实现 sort.Interface。
-type AccountsByURL []Account
+type AccountsByURL []Account // 定义 AccountsByURL 类型，它是一个 Account 的切片。
 
-func (a AccountsByURL) Len() int           { return len(a) }
-func (a AccountsByURL) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a AccountsByURL) Less(i, j int) bool { return a[i].URL.Cmp(a[j].URL) < 0 }
+// Len 返回切片的长度。
+func (a AccountsByURL) Len() int { return len(a) } // Len 方法返回切片 a 的长度。
+// Swap 交换切片中两个元素的位置。
+func (a AccountsByURL) Swap(i, j int) { a[i], a[j] = a[j], a[i] } // Swap 方法交换切片 a 中索引为 i 和 j 的两个元素。
+// Less 比较两个元素的 URL，用于排序。
+func (a AccountsByURL) Less(i, j int) bool { return a[i].URL.Cmp(a[j].URL) < 0 } // Less 方法比较切片 a 中索引为 i 和 j 的两个元素的 URL，如果第一个小于第二个，则返回 true。
 
 // WalletsByURL implements sort.Interface for []Wallet based on the URL field.
 // WalletsByURL 基于 URL 字段为 []Wallet 实现 sort.Interface。
-type WalletsByURL []Wallet
+type WalletsByURL []Wallet // 定义 WalletsByURL 类型，它是一个 Wallet 的切片。
 
-func (w WalletsByURL) Len() int           { return len(w) }
-func (w WalletsByURL) Swap(i, j int)      { w[i], w[j] = w[j], w[i] }
-func (w WalletsByURL) Less(i, j int) bool { return w[i].URL().Cmp(w[j].URL()) < 0 }
+// Len 返回切片的长度。
+func (w WalletsByURL) Len() int { return len(w) } // Len 方法返回切片 w 的长度。
+// Swap 交换切片中两个元素的位置。
+func (w WalletsByURL) Swap(i, j int) { w[i], w[j] = w[j], w[i] } // Swap 方法交换切片 w 中索引为 i 和 j 的两个元素。
+// Less 比较两个元素的 URL，用于排序。
+func (w WalletsByURL) Less(i, j int) bool { return w[i].URL().Cmp(w[j].URL()) < 0 } // Less 方法比较切片 w 中索引为 i 和 j 的两个元素的 URL，如果第一个小于第二个，则返回 true。
--- accounts/url.go
+++ accounts/url.go
@@ -14,13 +14,24 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// 版权所有 2017 The go-ethereum Authors
+// 此文件是 go-ethereum 库的一部分。
+//
+// go-ethereum 库是免费软件：您可以根据自由软件基金会发布的 GNU 宽通用公共许可证的条款重新分发和/或修改它，
+// 可以是许可证的第 3 版，也可以是（由您选择）任何更高版本。
+//
+// go-ethereum 库的发布是希望它能有用，但没有任何保证；甚至没有对适销性或特定用途适用性的默示保证。
+// 有关更多详细信息，请参阅 GNU 宽通用公共许可证。
+//
+// 您应该已经随 go-ethereum 库收到一份 GNU 宽通用公共许可证的副本。如果没有，请参阅 <http://www.gnu.org/licenses/>。
+
 package accounts
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"strings"
+	"encoding/json" // 导入 encoding/json 包，用于 JSON 数据的编码和解码。
+	"errors"        // 导入 errors 包，用于创建和处理错误。
+	"fmt"           // 导入 fmt 包，用于格式化字符串。
+	"strings"       // 导入 strings 包，用于处理字符串。
 )
 
 // URL represents the canonical identification URL of a wallet or account.
@@ -51,57 +62,57 @@ type URL struct {
 
 // parseURL converts a user supplied URL into the accounts specific structure.
 // parseURL 将用户提供的 URL 转换为特定于帐户的结构。
-func parseURL(url string) (URL, error) {
-	parts := strings.Split(url, "://")
-	if len(parts) != 2 || parts[0] == "" {
-		return URL{}, errors.New("protocol scheme missing")
+func parseURL(url string) (URL, error) { // 定义 parseURL 函数，用于解析 URL 字符串。
+	parts := strings.Split(url, "://") // 按 "://" 分割 URL 字符串。
+	if len(parts) != 2 || parts[0] == "" { // 如果分割后的部分不等于 2 或协议方案为空。
+		return URL{}, errors.New("protocol scheme missing") // 返回错误。
 	}
-	return URL{
-		Scheme: parts[0],
-		Path:   parts[1],
-	}, nil
+	return URL{ // 返回解析后的 URL 结构体。
+		Scheme: parts[0], // 设置协议方案。
+		Path:   parts[1], // 设置路径。
+	}, nil // 返回 nil 错误。
 }
 
 // String implements the stringer interface.
 // String 实现 stringer 接口。
-func (u URL) String() string {
-	if u.Scheme != "" {
-		return fmt.Sprintf("%s://%s", u.Scheme, u.Path)
+func (u URL) String() string { // 为 URL 类型定义 String 方法。
+	if u.Scheme != "" { // 如果协议方案不为空。
+		return fmt.Sprintf("%s://%s", u.Scheme, u.Path) // 返回格式化后的 URL 字符串。
 	}
-	return u.Path
+	return u.Path // 否则只返回路径。
 }
 
 // TerminalString implements the log.TerminalStringer interface.
 // TerminalString 实现 log.TerminalStringer 接口。
-func (u URL) TerminalString() string {
-	url := u.String()
-	if len(url) > 32 {
-		return url[:31] + ".."
+func (u URL) TerminalString() string { // 为 URL 类型定义 TerminalString 方法。
+	url := u.String() // 获取 URL 的字符串表示形式。
+	if len(url) > 32 { // 如果 URL 字符串长度超过 32。
+		return url[:31] + ".." // 截断并添加 ".."。
 	}
-	return url
+	return url // 返回 URL 字符串。
 }
 
 // MarshalJSON implements the json.Marshaller interface.
 // MarshalJSON 实现 json.Marshaller 接口。
-func (u URL) MarshalJSON() ([]byte, error) {
-	return json.Marshal(u.String())
+func (u URL) MarshalJSON() ([]byte, error) { // 为 URL 类型定义 MarshalJSON 方法。
+	return json.Marshal(u.String()) // 将 URL 的字符串表示形式进行 JSON 编码。
 }
 
 // UnmarshalJSON parses url.
 // UnmarshalJSON 解析 url。
-func (u *URL) UnmarshalJSON(input []byte) error {
-	var textURL string
-	err := json.Unmarshal(input, &textURL)
-	if err != nil {
-		return err
+func (u *URL) UnmarshalJSON(input []byte) error { // 为 URL 类型定义 UnmarshalJSON 方法。
+	var textURL string // 声明一个字符串变量 textURL。
+	err := json.Unmarshal(input, &textURL) // 解码 JSON 数据到 textURL。
+	if err != nil { // 如果解码失败。
+		return err // 返回错误。
 	}
-	url, err := parseURL(textURL)
-	if err != nil {
-		return err
+	url, err := parseURL(textURL) // 解析 URL 字符串。
+	if err != nil { // 如果解析失败。
+		return err // 返回错误。
 	}
-	u.Scheme = url.Scheme
-	u.Path = url.Path
-	return nil
+	u.Scheme = url.Scheme // 设置协议方案。
+	u.Path = url.Path       // 设置路径。
+	return nil             // 返回 nil 错误。
 }
 
 // Cmp compares x and y and returns:
@@ -115,9 +126,9 @@ func (u *URL) UnmarshalJSON(input []byte) error {
 //	-1 如果 x <  y
 //	 0 如果 x == y
 //	+1 如果 x >  y
-func (u URL) Cmp(url URL) int {
-	if u.Scheme == url.Scheme {
-		return strings.Compare(u.Path, url.Path)
+func (u URL) Cmp(url URL) int { // 为 URL 类型定义 Cmp 方法，用于比较两个 URL。
+	if u.Scheme == url.Scheme { // 如果协议方案相同。
+		return strings.Compare(u.Path, url.Path) // 比较路径。
 	}
-	return strings.Compare(u.Scheme, url.Scheme)
+	return strings.Compare(u.Scheme, url.Scheme) // 比较协议方案。
 }
