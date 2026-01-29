b02fab324465a9f872a66927f214dff7c953dd46
nidaye0525 <nidaye0525@gmail.com>
2026-01-29 09:09:06 +0000

    feat: add Chinese comments to accounts/abi/doc.go and accounts/abi/error.go

--- accounts/abi/doc.go
+++ accounts/abi/doc.go
@@ -14,6 +14,17 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// 版权所有 2015 The go-ethereum Authors
+// 此文件是 go-ethereum 库的一部分。
+//
+// go-ethereum 库是免费软件：您可以根据自由软件基金会发布的 GNU 宽通用公共许可证的条款重新分发和/或修改它，
+// 可以是许可证的第 3 版，也可以是（由您选择）任何更高版本。
+//
+// go-ethereum 库的发布是希望它能有用，但没有任何保证；甚至没有对适销性或特定用途适用性的默示保证。
+// 有关更多详细信息，请参阅 GNU 宽通用公共许可证。
+//
+// 您应该已经随 go-ethereum 库收到一份 GNU 宽通用公共许可证的副本。如果没有，请参阅 <http://www.gnu.org/licenses/>。
+
 // Package abi implements the Ethereum ABI (Application Binary
 // Interface).
 //
@@ -23,4 +34,10 @@
 // as unsigned slice to signed slice. Bit size type casting is also
 // handled. ints with a bit size of 32 will be properly cast to int256,
 // etc.
+// abi 包实现了以太坊 ABI（应用程序二进制接口）。
+//
+// 以太坊 ABI 是强类型的，在编译时已知并且是静态的。
+// 此 ABI 将处理基本的类型转换；无符号到有符号，反之亦然。
+// 它不处理切片转换，例如无符号切片到有符号切片。
+// 位大小类型转换也会处理。位大小为 32 的 int 将被正确转换为 int256，等等。
 package abi
--- accounts/abi/error.go
+++ accounts/abi/error.go
@@ -14,6 +14,17 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// 版权所有 2016 The go-ethereum Authors
+// 此文件是 go-ethereum 库的一部分。
+//
+// go-ethereum 库是免费软件：您可以根据自由软件基金会发布的 GNU 宽通用公共许可证的条款重新分发和/或修改它，
+// 可以是许可证的第 3 版，也可以是（由您选择）任何更高版本。
+//
+// go-ethereum 库的发布是希望它能有用，但没有任何保证；甚至没有对适销性或特定用途适用性的默示保证。
+// 有关更多详细信息，请参阅 GNU 宽通用公共许可证。
+//
+// 您应该已经随 go-ethereum 库收到一份 GNU 宽通用公共许可证的副本。如果没有，请参阅 <http://www.gnu.org/licenses/>。
+
 package abi
 
 import (
@@ -25,24 +36,25 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
+// Error 结构体表示 ABI 中的自定义错误。
 type Error struct {
 	Name   string
 	Inputs Arguments
-	str    string
+	str    string // 缓存的字符串表示形式
 
-	// Sig contains the string signature according to the ABI spec.
-	// e.g. error foo(uint32 a, int b) = "foo(uint32,int256)"
-	// Please note that "int" is substitute for its canonical representation "int256"
+	// Sig 包含根据 ABI 规范的字符串签名。
+	// 例如: error foo(uint32 a, int b) = "foo(uint32,int256)"
+	// 请注意，“int”会替换为其规范表示“int256”。
 	Sig string
 
-	// ID returns the canonical representation of the error's signature used by the
-	// abi definition to identify event names and types.
+	// ID 返回错误的规范表示形式，abi 定义使用它来识别事件名称和类型。
 	ID common.Hash
 }
 
+// NewError 创建一个新的 Error 对象。
 func NewError(name string, inputs Arguments) Error {
-	// sanitize inputs to remove inputs without names
-	// and precompute string and sig representation.
+	// 清理输入，以删除没有名称的输入，
+	// 并预先计算字符串和签名表示形式。
 	names := make([]string, len(inputs))
 	types := make([]string, len(inputs))
 	for i, input := range inputs {
@@ -55,17 +67,18 @@ func NewError(name string, inputs Arguments) Error {
 		} else {
 			inputs[i] = input
 		}
-		// string representation
+		// 字符串表示形式
 		names[i] = fmt.Sprintf("%v %v", input.Type, inputs[i].Name)
 		if input.Indexed {
 			names[i] = fmt.Sprintf("%v indexed %v", input.Type, inputs[i].Name)
 		}
-		// sig representation
+		// 签名表示形式
 		types[i] = input.Type.String()
 	}
 
 	str := fmt.Sprintf("error %v(%v)", name, strings.Join(names, ", "))
 	sig := fmt.Sprintf("%v(%v)", name, strings.Join(types, ","))
+	// 错误签名的 Keccak256 哈希值作为 ID
 	id := common.BytesToHash(crypto.Keccak256([]byte(sig)))
 
 	return Error{
@@ -77,10 +90,13 @@ func NewError(name string, inputs Arguments) Error {
 	}
 }
 
+// String 返回错误的字符串表示形式。
 func (e Error) String() string {
 	return e.str
 }
 
+// Unpack 将给定的数据解包为合适的值。
+// 它首先检查 4 字节的错误 ID，然后解包参数。
 func (e *Error) Unpack(data []byte) (interface{}, error) {
 	if len(data) < 4 {
 		return "", fmt.Errorf("insufficient data for unpacking: have %d, want at least 4", len(data))
