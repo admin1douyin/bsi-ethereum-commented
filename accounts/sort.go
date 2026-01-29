d96140e10668c6e561a080a1de7dc29d9a3d6f52
nidaye0525 <nidaye0525@gmail.com>
2026-01-29 03:39:03 +0000

    docs: add Chinese comments to accounts/sort.go

--- accounts/sort.go
+++ accounts/sort.go
@@ -17,6 +17,7 @@
 package accounts
 
 // AccountsByURL implements sort.Interface for []Account based on the URL field.
+// AccountsByURL 基于 URL 字段为 []Account 实现 sort.Interface。
 type AccountsByURL []Account
 
 func (a AccountsByURL) Len() int           { return len(a) }
@@ -24,6 +25,7 @@ func (a AccountsByURL) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a AccountsByURL) Less(i, j int) bool { return a[i].URL.Cmp(a[j].URL) < 0 }
 
 // WalletsByURL implements sort.Interface for []Wallet based on the URL field.
+// WalletsByURL 基于 URL 字段为 []Wallet 实现 sort.Interface。
 type WalletsByURL []Wallet
 
 func (w WalletsByURL) Len() int           { return len(w) }
