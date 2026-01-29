468afe5360d2e73e2d5a1b6c66763d448f28038d
nidaye0525 <nidaye0525@gmail.com>
2026-01-29 09:55:45 +0000

    feat: add Chinese comments to accounts/abi/type.go and accounts/abi/unpack.go

--- accounts/abi/type.go
+++ accounts/abi/type.go
@@ -14,6 +14,17 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// 版权所有 2015 The go-ethereum Authors
+// 此文件是 go-ethereum 库的一部分。
+//
+// go-ethereum 库是免费软件：您可以根据自由软件基金会发布的 GNU 宽通用公共许可证的条款重新分发和/或修改它，
+// 可以是许可证的第 3 版，也可以是（由您选择）任何更高版本。
+//
+// go-ethereum 库的发布是希望它能有用，但没有任何保证；甚至没有对适销性或特定用途适用性的默示保证。
+// 有关更多详细信息，请参阅 GNU 宽通用公共许可证。
+//
+// 您应该已经随 go-ethereum 库收到一份 GNU 宽通用公共许可证的副本。如果没有，请参阅 <http://www.gnu.org/licenses/>。
+
 package abi
 
 import (
@@ -30,114 +41,132 @@ import (
 )
 
 // Type enumerator
+// 类型枚举器
 const (
-	IntTy byte = iota
-	UintTy
-	BoolTy
-	StringTy
-	SliceTy
-	ArrayTy
-	TupleTy
-	AddressTy
-	FixedBytesTy
-	BytesTy
-	HashTy
-	FixedPointTy
-	FunctionTy
+	IntTy        byte = iota // 有符号整型
+	UintTy                     // 无符号整型
+	BoolTy                     // 布尔型
+	StringTy                   // 字符串
+	SliceTy                    // 切片
+	ArrayTy                    // 数组
+	TupleTy                    // 元组 (结构体)
+	AddressTy                  // 地址类型
+	FixedBytesTy               // 固定长度字节数组
+	BytesTy                    // 动态长度字节数组
+	HashTy                     // 哈希类型
+	FixedPointTy               // 定点数类型
+	FunctionTy                 // 函数类型
 )
 
 // Type is the reflection of the supported argument type.
+// Type 是支持的参数类型的反射表示。
 type Type struct {
-	Elem *Type
-	Size int
-	T    byte // Our own type checking
+	Elem *Type // 嵌套元素类型（用于数组/切片）
+	Size int    // 类型大小（例如 uint256 的 size 是 256，bytes32 的 size 是 32）
+	T    byte   // 我们自己的类型检查，使用上面的枚举器
 
-	stringKind string // holds the unparsed string for deriving signatures
+	stringKind string // 保存用于派生签名的未解析字符串
 
 	// Tuple relative fields
-	TupleRawName  string       // Raw struct name defined in source code, may be empty.
-	TupleElems    []*Type      // Type information of all tuple fields
-	TupleRawNames []string     // Raw field name of all tuple fields
-	TupleType     reflect.Type // Underlying struct of the tuple
+	// 元组相关字段
+	TupleRawName  string       // 源代码中定义的原始结构体名称，可能为空。
+	TupleElems    []*Type      // 所有元组字段的类型信息
+	TupleRawNames []string     // 所有元组字段的原始字段名称
+	TupleType     reflect.Type // 元组的底层结构体类型
 }
 
 var (
 	// typeRegex parses the abi sub types
+	// typeRegex 解析 abi 子类型
 	typeRegex = regexp.MustCompile("([a-zA-Z]+)(([0-9]+)(x([0-9]+))?)?")
 
 	// sliceSizeRegex grab the slice size
+	// sliceSizeRegex 获取切片/数组的大小
 	sliceSizeRegex = regexp.MustCompile("[0-9]+")
 )
 
 // NewType creates a new reflection type of abi type given in t.
+// NewType 根据给定的 t 创建一个新的 abi 类型的反射类型。
 func NewType(t string, internalType string, components []ArgumentMarshaling) (typ Type, err error) {
 	// check that array brackets are equal if they exist
+	// 检查数组的方括号是否匹配
 	if strings.Count(t, "[") != strings.Count(t, "]") {
-		return Type{}, errors.New("invalid arg type in abi")
+		return Type{}, errors.New("invalid arg type in abi") // abi 中无效的参数类型
 	}
 	typ.stringKind = t
 
 	// if there are brackets, get ready to go into slice/array mode and
 	// recursively create the type
+	// 如果有方括号，就进入切片/数组模式并递归地创建类型
 	if strings.Count(t, "[") != 0 {
 		// Note internalType can be empty here.
+		// 注意 internalType 在这里可能为空。
 		subInternal := internalType
 		if i := strings.LastIndex(internalType, "["); i != -1 {
 			subInternal = subInternal[:i]
 		}
 		// recursively embed the type
+		// 递归地嵌入类型
 		i := strings.LastIndex(t, "[")
 		embeddedType, err := NewType(t[:i], subInternal, components)
 		if err != nil {
 			return Type{}, err
 		}
 		// grab the last cell and create a type from there
+		// 获取最后一个单元并从中创建类型
 		sliced := t[i:]
 		// grab the slice size with regexp
+		// 用正则表达式获取切片大小
 		intz := sliceSizeRegex.FindAllString(sliced, -1)
 
 		if len(intz) == 0 {
-			// is a slice
+			// is a slice (e.g., "[]")
+			// 是一个切片
 			typ.T = SliceTy
 			typ.Elem = &embeddedType
 			typ.stringKind = embeddedType.stringKind + sliced
 		} else if len(intz) == 1 {
-			// is an array
+			// is an array (e.g., "[3]")
+			// 是一个数组
 			typ.T = ArrayTy
 			typ.Elem = &embeddedType
 			typ.Size, err = strconv.Atoi(intz[0])
 			if err != nil {
-				return Type{}, fmt.Errorf("abi: error parsing variable size: %v", err)
+				return Type{}, fmt.Errorf("abi: error parsing variable size: %v", err) // abi：解析可变大小时出错
 			}
 			typ.stringKind = embeddedType.stringKind + sliced
 		} else {
-			return Type{}, errors.New("invalid formatting of array type")
+			return Type{}, errors.New("invalid formatting of array type") // 无效的数组类型格式
 		}
 		return typ, err
 	}
 	// parse the type and size of the abi-type.
+	// 解析 abi 类型的类型和大小。
 	matches := typeRegex.FindAllStringSubmatch(t, -1)
 	if len(matches) == 0 {
-		return Type{}, fmt.Errorf("invalid type '%v'", t)
+		return Type{}, fmt.Errorf("invalid type '%v'", t) // 无效类型
 	}
 	parsedType := matches[0]
 
 	// varSize is the size of the variable
+	// varSize 是变量的大小
 	var varSize int
 	if len(parsedType[3]) > 0 {
 		var err error
 		varSize, err = strconv.Atoi(parsedType[2])
 		if err != nil {
-			return Type{}, fmt.Errorf("abi: error parsing variable size: %v", err)
+			return Type{}, fmt.Errorf("abi: error parsing variable size: %v", err) // abi: 解析变量大小时出错
 		}
 	} else {
 		if parsedType[0] == "uint" || parsedType[0] == "int" {
 			// this should fail because it means that there's something wrong with
 			// the abi type (the compiler should always format it to the size...always)
-			return Type{}, fmt.Errorf("unsupported arg type: %s", t)
+			// 这应该会失败，因为这意味着 abi 类型有问题（编译器应该总是将其格式化为大小...总是）
+			return Type{}, fmt.Errorf("unsupported arg type: %s", t) // 不支持的参数类型
 		}
 	}
 	// varType is the parsed abi type
+	// varType 是解析后的 abi 类型
 	switch varType := parsedType[1]; varType {
 	case "int":
 		typ.Size = varSize
@@ -154,21 +183,23 @@ func NewType(t string, internalType string, components []ArgumentMarshaling) (ty
 		typ.T = StringTy
 	case "bytes":
 		if varSize == 0 {
+			// 动态大小的 bytes
 			typ.T = BytesTy
 		} else {
 			if varSize > 32 {
-				return Type{}, fmt.Errorf("unsupported arg type: %s", t)
+				return Type{}, fmt.Errorf("unsupported arg type: %s", t) // 不支持的参数类型
 			}
+			// 固定大小的 bytes, e.g., bytes32
 			typ.T = FixedBytesTy
 			typ.Size = varSize
 		}
 	case "tuple":
 		var (
-			fields     []reflect.StructField
-			elems      []*Type
-			names      []string
-			expression string // canonical parameter expression
-			used       = make(map[string]bool)
+			fields     []reflect.StructField // Go 结构体字段
+			elems      []*Type             // 元组元素的 ABI 类型
+			names      []string            // 元组元素的原始名称
+			expression string              // 规范的参数表达式, e.g., "(uint256,string)"
+			used       = make(map[string]bool) // 用于处理字段名冲突
 		)
 		expression += "("
 		for idx, c := range components {
@@ -178,15 +209,15 @@ func NewType(t string, internalType string, components []ArgumentMarshaling) (ty
 			}
 			name := ToCamelCase(c.Name)
 			if name == "" {
-				return Type{}, errors.New("abi: purely anonymous or underscored field is not supported")
+				return Type{}, errors.New("abi: purely anonymous or underscored field is not supported") // abi: 不支持纯匿名或下划线开头的字段
 			}
 			fieldName := ResolveNameConflict(name, func(s string) bool { return used[s] })
 			used[fieldName] = true
 			if !isValidFieldName(fieldName) {
-				return Type{}, fmt.Errorf("field %d has invalid name", idx)
+				return Type{}, fmt.Errorf("field %d has invalid name", idx) // 字段 %d 的名称无效
 			}
 			fields = append(fields, reflect.StructField{
-				Name: fieldName, // reflect.StructOf will panic for any exported field.
+				Name: fieldName, // reflect.StructOf 对任何未导出的字段都会 panic。
 				Type: cType.GetType(),
 				Tag:  reflect.StructTag("json:\"" + c.Name + "\""),
 			})
@@ -209,21 +240,26 @@ func NewType(t string, internalType string, components []ArgumentMarshaling) (ty
 		// After solidity 0.5.10, a new field of abi "internalType"
 		// is introduced. From that we can obtain the struct name
 		// user defined in the source code.
+		// 在 solidity 0.5.10 之后，引入了一个新的 abi 字段 "internalType"。
+		// 从中我们可以获取用户在源代码中定义的结构体名称。
 		if internalType != "" && strings.HasPrefix(internalType, structPrefix) {
 			// Foo.Bar type definition is not allowed in golang,
 			// convert the format to FooBar
+			// Go 中不允许 Foo.Bar 这样的类型定义，
+			// 将其格式转换为 FooBar
 			typ.TupleRawName = strings.ReplaceAll(internalType[len(structPrefix):], ".", "")
 		}
 
 	case "function":
 		typ.T = FunctionTy
-		typ.Size = 24
+		typ.Size = 24 // 20 字节地址 + 4 字节函数选择器
 	default:
 		if strings.HasPrefix(internalType, "contract ") {
+			// 合约类型在 ABI 中表示为地址
 			typ.Size = 20
 			typ.T = AddressTy
 		} else {
-			return Type{}, fmt.Errorf("unsupported arg type: %s", t)
+			return Type{}, fmt.Errorf("unsupported arg type: %s", t) // 不支持的参数类型
 		}
 	}
 
@@ -231,6 +267,7 @@ func NewType(t string, internalType string, components []ArgumentMarshaling) (ty
 }
 
 // GetType returns the reflection type of the ABI type.
+// GetType 返回 ABI 类型的 Go 反射类型。
 func (t Type) GetType() reflect.Type {
 	switch t.T {
 	case IntTy:
@@ -253,22 +290,25 @@ func (t Type) GetType() reflect.Type {
 		return reflect.ArrayOf(t.Size, reflect.TypeFor[byte]())
 	case BytesTy:
 		return reflect.TypeFor[[]byte]()
-	case HashTy, FixedPointTy: // currently not used
+	case HashTy, FixedPointTy: // currently not used (当前未使用)
 		return reflect.TypeFor[[32]byte]()
 	case FunctionTy:
 		return reflect.TypeFor[[24]byte]()
 	default:
-		panic("Invalid type")
+		panic("Invalid type") // 无效类型
 	}
 }
 
 // String implements Stringer.
+// String 实现了 Stringer 接口，返回类型的字符串表示形式。
 func (t Type) String() (out string) {
 	return t.stringKind
 }
 
+// pack 对值 v 进行 ABI 编码
 func (t Type) pack(v reflect.Value) ([]byte, error) {
 	// dereference pointer first if it's a pointer
+	// 如果是指针，首先解引用
 	v = indirect(v)
 	if err := typeCheck(t, v); err != nil {
 		return nil, err
@@ -280,25 +320,30 @@ func (t Type) pack(v reflect.Value) ([]byte, error) {
 
 		if t.requiresLengthPrefix() {
 			// append length
+			// 对动态数组/切片，前缀添加长度
 			ret = append(ret, packNum(reflect.ValueOf(v.Len()))...)
 		}
 
 		// calculate offset if any
+		// 计算偏移量（如果需要的话）
 		offset := 0
+		// 如果元素是动态类型，则需要计算偏移量
 		offsetReq := isDynamicType(*t.Elem)
 		if offsetReq {
 			offset = getTypeSize(*t.Elem) * v.Len()
 		}
-		var tail []byte
+		var tail []byte // 用于存储动态类型的数据
 		for i := 0; i < v.Len(); i++ {
 			val, err := t.Elem.pack(v.Index(i))
 			if err != nil {
 				return nil, err
 			}
 			if !offsetReq {
+				// 如果是静态类型，直接追加编码后的数据
 				ret = append(ret, val...)
 				continue
 			}
+			// 如果是动态类型，先追加偏移量，然后将数据暂存到 tail
 			ret = append(ret, packNum(reflect.ValueOf(offset))...)
 			offset += len(val)
 			tail = append(tail, val...)
@@ -314,11 +359,16 @@ func (t Type) pack(v reflect.Value) ([]byte, error) {
 		//     head(X(i)) = enc(len(head(X(1)) ... head(X(k)) tail(X(1)) ... tail(X(i-1))))
 		//     tail(X(i)) = enc(X(i))
 		// otherwise, i.e. if Ti is a dynamic type.
+		// 元组编码规则：
+		// 编码结果是 head(X(1))...head(X(k))tail(X(1))...tail(X(k))
+		// 对于静态类型 Ti，head(X(i)) = enc(X(i))，tail(X(i)) 为空
+		// 对于动态类型 Ti，head(X(i)) 是一个偏移量，指向 tail(X(i)) 的起始位置，而 tail(X(i)) = enc(X(i))
 		fieldmap, err := mapArgNamesToStructFields(t.TupleRawNames, v)
 		if err != nil {
 			return nil, err
 		}
 		// Calculate prefix occupied size.
+		// 计算头部占用的总大小。
 		offset := 0
 		for _, elem := range t.TupleElems {
 			offset += getTypeSize(*elem)
@@ -327,17 +377,19 @@ func (t Type) pack(v reflect.Value) ([]byte, error) {
 		for i, elem := range t.TupleElems {
 			field := v.FieldByName(fieldmap[t.TupleRawNames[i]])
 			if !field.IsValid() {
-				return nil, fmt.Errorf("field %s for tuple not found in the given struct", t.TupleRawNames[i])
+				return nil, fmt.Errorf("field %s for tuple not found in the given struct", t.TupleRawNames[i]) // 在给定的结构体中找不到元组的字段 %s
 			}
 			val, err := elem.pack(field)
 			if err != nil {
 				return nil, err
 			}
 			if isDynamicType(*elem) {
+				// 如果是动态类型，在头部放入偏移量，将实际数据追加到尾部
 				ret = append(ret, packNum(reflect.ValueOf(offset))...)
 				tail = append(tail, val...)
 				offset += len(val)
 			} else {
+				// 如果是静态类型，直接将数据放入头部
 				ret = append(ret, val...)
 			}
 		}
@@ -350,6 +402,7 @@ func (t Type) pack(v reflect.Value) ([]byte, error) {
 
 // requiresLengthPrefix returns whether the type requires any sort of length
 // prefixing.
+// requiresLengthPrefix 返回该类型是否需要任何形式的长度前缀。
 func (t Type) requiresLengthPrefix() bool {
 	return t.T == StringTy || t.T == BytesTy || t.T == SliceTy
 }
@@ -361,6 +414,13 @@ func (t Type) requiresLengthPrefix() bool {
 // * T[] for any T
 // * T[k] for any dynamic T and any k >= 0
 // * (T1,...,Tk) if Ti is dynamic for some 1 <= i <= k
+// isDynamicType 检查一个类型是否是动态的。
+// 以下类型被称为“动态类型”：
+// * bytes
+// * string
+// * 任意 T 的 T[]
+// * 任意动态 T 和任意 k >= 0 的 T[k]
+// * (T1,...,Tk)，如果对于某个 1 <= i <= k，Ti 是动态的
 func isDynamicType(t Type) bool {
 	if t.T == TupleTy {
 		for _, elem := range t.TupleElems {
@@ -381,25 +441,35 @@ func isDynamicType(t Type) bool {
 // variable actually occupies.
 // For a dynamic variable, the returned size is fixed 32 bytes, which is used
 // to store the location reference for actual value storage.
+// getTypeSize 返回此类型在 ABI 编码的头部所占用的字节大小。
+// 我们区分静态类型和动态类型。静态类型是原地编码的，
+// 而动态类型是在当前块之后的一个单独分配的位置进行编码的。
+// 因此，对于静态变量，返回的大小表示变量实际占用的空间。
+// 对于动态变量，返回的大小是固定的 32 字节，用于存储指向实际值存储位置的引用（偏移量）。
 func getTypeSize(t Type) int {
 	if t.T == ArrayTy && !isDynamicType(*t.Elem) {
 		// Recursively calculate type size if it is a nested array
+		// 如果是嵌套数组，则递归计算类型大小
 		if t.Elem.T == ArrayTy || t.Elem.T == TupleTy {
 			return t.Size * getTypeSize(*t.Elem)
 		}
 		return t.Size * 32
 	} else if t.T == TupleTy && !isDynamicType(t) {
+		// 静态元组的大小是其所有元素大小的总和
 		total := 0
 		for _, elem := range t.TupleElems {
 			total += getTypeSize(*elem)
 		}
 		return total
 	}
+	// 动态类型和基本静态类型在头部都占用 32 字节
 	return 32
 }
 
 // isLetter reports whether a given 'rune' is classified as a Letter.
 // This method is copied from reflect/type.go
+// isLetter 报告给定的 'rune' 是否被归类为字母。
+// 此方法从 reflect/type.go 复制而来。
 func isLetter(ch rune) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_' || ch >= utf8.RuneSelf && unicode.IsLetter(ch)
 }
@@ -411,6 +481,13 @@ func isLetter(ch rune) bool {
 // identifier = letter { letter | unicode_digit } .
 // letter = unicode_letter | "_" .
 // This method is copied from reflect/type.go
+// isValidFieldName 检查一个字符串是否是有效的（结构体）字段名。
+//
+// 根据语言规范，字段名应该是一个标识符。
+//
+// identifier = letter { letter | unicode_digit } .
+// letter = unicode_letter | "_" .
+// 此方法从 reflect/type.go 复制而来。
 func isValidFieldName(fieldName string) bool {
 	for i, c := range fieldName {
 		if i == 0 && !isLetter(c) {
--- accounts/abi/unpack.go
+++ accounts/abi/unpack.go
@@ -14,6 +14,17 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// 版权所有 2017 The go-ethereum Authors
+// 此文件是 go-ethereum 库的一部分。
+//
+// go-ethereum 库是免费软件：您可以根据自由软件基金会发布的 GNU 宽通用公共许可证的条款重新分发和/或修改它，
+// 可以是许可证的第 3 版，也可以是（由您选择）任何更高版本。
+//
+// go-ethereum 库的发布是希望它能有用，但没有任何保证；甚至没有对适销性或特定用途适用性的默示保证。
+// 有关更多详细信息，请参阅 GNU 宽通用公共许可证。
+//
+// 您应该已经随 go-ethereum 库收到一份 GNU 宽通用公共许可证的副本。如果没有，请参阅 <http://www.gnu.org/licenses/>。
+
 package abi
 
 import (
@@ -29,16 +40,21 @@ import (
 
 var (
 	// MaxUint256 is the maximum value that can be represented by a uint256.
+	// MaxUint256 是 uint256 能表示的最大值 (2^256 - 1)。
 	MaxUint256 = new(big.Int).Sub(new(big.Int).Lsh(common.Big1, 256), common.Big1)
 	// MaxInt256 is the maximum value that can be represented by a int256.
+	// MaxInt256 是 int256 能表示的最大值 (2^255 - 1)。
 	MaxInt256 = new(big.Int).Sub(new(big.Int).Lsh(common.Big1, 255), common.Big1)
 )
 
 // ReadInteger reads the integer based on its kind and returns the appropriate value.
+// ReadInteger 根据其类型读取整数并返回适当的值。
 func ReadInteger(typ Type, b []byte) (interface{}, error) {
+	// 从字节创建大整数
 	ret := new(big.Int).SetBytes(b)
 
 	if typ.T == UintTy {
+		// 如果是无符号整数
 		u64, isu64 := ret.Uint64(), ret.IsUint64()
 		switch typ.Size {
 		case 8:
@@ -63,6 +79,7 @@ func ReadInteger(typ Type, b []byte) (interface{}, error) {
 			return u64, nil
 		default:
 			// the only case left for unsigned integer is uint256.
+			// 无符号整数剩下的唯一情况是 uint256。
 			return ret, nil
 		}
 	}
@@ -70,7 +87,11 @@ func ReadInteger(typ Type, b []byte) (interface{}, error) {
 	// big.SetBytes can't tell if a number is negative or positive in itself.
 	// On EVM, if the returned number > max int256, it is negative.
 	// A number is > max int256 if the bit at position 255 is set.
+	// big.SetBytes 本身无法判断一个数是正数还是负数。
+	// 在 EVM 中，如果返回的数字 > max int256，则它是一个负数。
+	// 如果第 255 位被设置，则该数字 > max int256。
 	if ret.Bit(255) == 1 {
+		// 这是处理负数的技巧，使用补码表示
 		ret.Add(MaxUint256, new(big.Int).Neg(ret))
 		ret.Add(ret, common.Big1)
 		ret.Neg(ret)
@@ -99,18 +120,21 @@ func ReadInteger(typ Type, b []byte) (interface{}, error) {
 		return i64, nil
 	default:
 		// the only case left for integer is int256
-
+		// 有符号整数剩下的唯一情况是 int256。
 		return ret, nil
 	}
 }
 
 // readBool reads a bool.
+// readBool 从一个 32 字节的 word 中读取一个布尔值。
 func readBool(word []byte) (bool, error) {
+	// 检查前 31 个字节是否都为 0
 	for _, b := range word[:31] {
 		if b != 0 {
 			return false, errBadBool
 		}
 	}
+	// 检查最后一个字节
 	switch word[31] {
 	case 0:
 		return false, nil
@@ -124,10 +148,14 @@ func readBool(word []byte) (bool, error) {
 // A function type is simply the address with the function selection signature at the end.
 //
 // readFunctionType enforces that standard by always presenting it as a 24-array (address + sig = 24 bytes)
+// 函数类型就是地址后跟函数选择器签名。
+//
+// readFunctionType 通过始终将其表示为 24 字节数组（地址 + 签名 = 24 字节）来强制执行该标准。
 func readFunctionType(t Type, word []byte) (funcTy [24]byte, err error) {
 	if t.T != FunctionTy {
 		return [24]byte{}, errors.New("abi: invalid type in call to make function type byte array")
 	}
+	// 检查最后 8 个字节是否为 0，因为函数类型只占用 24 个字节
 	if garbage := binary.BigEndian.Uint64(word[24:32]); garbage != 0 {
 		err = fmt.Errorf("abi: got improperly encoded function type, got %v", word)
 	} else {
@@ -137,18 +165,22 @@ func readFunctionType(t Type, word []byte) (funcTy [24]byte, err error) {
 }
 
 // ReadFixedBytes uses reflection to create a fixed array to be read from.
+// ReadFixedBytes 使用反射创建一个固定大小的数组并从中读取数据。
 func ReadFixedBytes(t Type, word []byte) (interface{}, error) {
 	if t.T != FixedBytesTy {
 		return nil, errors.New("abi: invalid type in call to make fixed byte array")
 	}
 	// convert
+	// 创建一个新的类型实例
 	array := reflect.New(t.GetType()).Elem()
 
+	// 将数据复制到新创建的数组中
 	reflect.Copy(array, reflect.ValueOf(word[0:t.Size]))
 	return array.Interface(), nil
 }
 
 // forEachUnpack iteratively unpack elements.
+// forEachUnpack 迭代地解包元素（用于数组和切片）。
 func forEachUnpack(t Type, output []byte, start, size int) (interface{}, error) {
 	if size < 0 {
 		return nil, fmt.Errorf("cannot marshal input to array, size is negative (%d)", size)
@@ -158,14 +190,17 @@ func forEachUnpack(t Type, output []byte, start, size int) (interface{}, error) 
 	}
 
 	// this value will become our slice or our array, depending on the type
+	// 根据类型，此值将成为我们的切片或数组
 	var refSlice reflect.Value
 
 	switch t.T {
 	case SliceTy:
 		// declare our slice
+		// 声明我们的切片
 		refSlice = reflect.MakeSlice(t.GetType(), size, size)
 	case ArrayTy:
 		// declare our array
+		// 声明我们的数组
 		refSlice = reflect.New(t.GetType()).Elem()
 	default:
 		return nil, errors.New("abi: invalid type in array/slice unpacking stage")
@@ -173,26 +208,34 @@ func forEachUnpack(t Type, output []byte, start, size int) (interface{}, error) 
 
 	// Arrays have packed elements, resulting in longer unpack steps.
 	// Slices have just 32 bytes per element (pointing to the contents).
+	// 静态数组的元素是紧密打包的，导致解包步骤更长。
+	// 动态数组/切片的元素每个都是 32 字节（指向内容）。
 	elemSize := getTypeSize(*t.Elem)
 
 	for i, j := start, 0; j < size; i, j = i+elemSize, j+1 {
+		// 递归解包每个元素
 		inter, err := toGoType(i, *t.Elem, output)
 		if err != nil {
 			return nil, err
 		}
 
 		// append the item to our reflect slice
+		// 将解包后的项添加到我们的反射切片/数组中
 		refSlice.Index(j).Set(reflect.ValueOf(inter))
 	}
 
 	// return the interface
+	// 返回接口
 	return refSlice.Interface(), nil
 }
 
+// forTupleUnpack 解包元组类型
 func forTupleUnpack(t Type, output []byte) (interface{}, error) {
+	// 创建一个新的元组（结构体）实例
 	retval := reflect.New(t.GetType()).Elem()
-	virtualArgs := 0
+	virtualArgs := 0 // 用于计算静态数组等占用的额外槽位
 	for index, elem := range t.TupleElems {
+		// 解包元组的每个元素
 		marshalledValue, err := toGoType((index+virtualArgs)*32, *elem, output)
 		if err != nil {
 			return nil, err
@@ -208,12 +251,24 @@ func forTupleUnpack(t Type, output []byte) (interface{}, error) {
 			//
 			// Calculate the full array size to get the correct offset for the next argument.
 			// Decrement it by 1, as the normal index increment is still applied.
+			// 如果我们有一个静态数组，比如 [3]uint256，它们被编码为
+			// uint256,uint256,uint256。
+			// 这意味着从现在开始计算索引时，我们需要添加两个“虚拟”参数。
+			//
+			// 嵌套多层的数组值也以内联方式编码：
+			// [2][3]uint256: uint256,uint256,uint256,uint256,uint256,uint256
+			//
+			// 计算完整的数组大小以获取下一个参数的正确偏移量。
+			// 将其减 1，因为正常的索引增量仍然适用。
 			virtualArgs += getTypeSize(*elem)/32 - 1
 		} else if elem.T == TupleTy && !isDynamicType(*elem) {
 			// If we have a static tuple, like (uint256, bool, uint256), these are
 			// coded as just like uint256,bool,uint256
+			// 如果我们有一个静态元组，比如 (uint256, bool, uint256)，它们被编码为
+			// uint256,bool,uint256
 			virtualArgs += getTypeSize(*elem)/32 - 1
 		}
+		// 设置结构体字段的值
 		retval.Field(index).Set(reflect.ValueOf(marshalledValue))
 	}
 	return retval.Interface(), nil
@@ -221,49 +276,58 @@ func forTupleUnpack(t Type, output []byte) (interface{}, error) {
 
 // toGoType parses the output bytes and recursively assigns the value of these bytes
 // into a go type with accordance with the ABI spec.
+// toGoType 解析输出字节，并根据 ABI 规范将这些字节的值递归地分配给 Go 类型。
 func toGoType(index int, t Type, output []byte) (interface{}, error) {
 	if index+32 > len(output) {
 		return nil, fmt.Errorf("abi: cannot marshal in to go type: length insufficient %d require %d", len(output), index+32)
 	}
 
 	var (
-		returnOutput  []byte
-		begin, length int
+		returnOutput  []byte // 当前要处理的 32 字节数据
+		begin, length int    // 对于动态类型，表示数据的起始位置和长度
 		err           error
 	)
 
 	// if we require a length prefix, find the beginning word and size returned.
+	// 如果需要长度前缀（动态类型），找到返回的起始词和大小。
 	if t.requiresLengthPrefix() {
 		begin, length, err = lengthPrefixPointsTo(index, output)
 		if err != nil {
 			return nil, err
 		}
 	} else {
+		// 静态类型直接取 32 字节
 		returnOutput = output[index : index+32]
 	}
 
 	switch t.T {
 	case TupleTy:
 		if isDynamicType(t) {
+			// 动态元组，需要先找到其数据位置
 			begin, err := tuplePointsTo(index, output)
 			if err != nil {
 				return nil, err
 			}
 			return forTupleUnpack(t, output[begin:])
 		}
+		// 静态元组，原地解包
 		return forTupleUnpack(t, output[index:])
 	case SliceTy:
+		// 动态切片
 		return forEachUnpack(t, output[begin:], 0, length)
 	case ArrayTy:
 		if isDynamicType(*t.Elem) {
+			// 动态元素的数组
 			offset := binary.BigEndian.Uint64(returnOutput[len(returnOutput)-8:])
 			if offset > uint64(len(output)) {
 				return nil, fmt.Errorf("abi: toGoType offset greater than output length: offset: %d, len(output): %d", offset, len(output))
 			}
 			return forEachUnpack(t, output[offset:], 0, t.Size)
 		}
+		// 静态元素的数组
 		return forEachUnpack(t, output[index:], 0, t.Size)
 	case StringTy: // variable arrays are written at the end of the return bytes
+		// 可变数组（字符串）写在返回字节的末尾
 		return string(output[begin : begin+length]), nil
 	case IntTy, UintTy:
 		return ReadInteger(t, returnOutput)
@@ -285,7 +349,9 @@ func toGoType(index int, t Type, output []byte) (interface{}, error) {
 }
 
 // lengthPrefixPointsTo interprets a 32 byte slice as an offset and then determines which indices to look to decode the type.
+// lengthPrefixPointsTo 将一个 32 字节的切片解释为偏移量，然后确定要查找哪些索引来解码类型。
 func lengthPrefixPointsTo(index int, output []byte) (start int, length int, err error) {
+	// 获取数据区的偏移量
 	bigOffsetEnd := new(big.Int).SetBytes(output[index : index+32])
 	bigOffsetEnd.Add(bigOffsetEnd, common.Big32)
 	outputLength := big.NewInt(int64(len(output)))
@@ -298,9 +364,11 @@ func lengthPrefixPointsTo(index int, output []byte) (start int, length int, err 
 		return 0, 0, fmt.Errorf("abi offset larger than int64: %v", bigOffsetEnd)
 	}
 
+	// 偏移量指向的位置是长度信息
 	offsetEnd := int(bigOffsetEnd.Uint64())
 	lengthBig := new(big.Int).SetBytes(output[offsetEnd-32 : offsetEnd])
 
+	// 检查总长度是否超出范围
 	totalSize := new(big.Int).Add(bigOffsetEnd, lengthBig)
 	if totalSize.BitLen() > 63 {
 		return 0, 0, fmt.Errorf("abi: length larger than int64: %v", totalSize)
@@ -309,13 +377,17 @@ func lengthPrefixPointsTo(index int, output []byte) (start int, length int, err 
 	if totalSize.Cmp(outputLength) > 0 {
 		return 0, 0, fmt.Errorf("abi: cannot marshal in to go type: length insufficient %v require %v", outputLength, totalSize)
 	}
+	// 数据的起始位置
 	start = int(bigOffsetEnd.Uint64())
+	// 数据的长度
 	length = int(lengthBig.Uint64())
 	return
 }
 
 // tuplePointsTo resolves the location reference for dynamic tuple.
+// tuplePointsTo 解析动态元组的位置引用。
 func tuplePointsTo(index int, output []byte) (start int, err error) {
+	// 从索引位置读取偏移量
 	offset := new(big.Int).SetBytes(output[index : index+32])
 	outputLen := big.NewInt(int64(len(output)))
 
